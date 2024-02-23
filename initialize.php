f9ab4acd49592f20d9b5586a7cbc20e39aa88a1f
vasim9399 <155420565+vasim9399@users.noreply.github.com>
2024-02-23 11:57:05 +0530

    Update initialize.php
    updated the connection

--- initialize.php
+++ initialize.php
@@ -3,8 +3,8 @@ $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=
 if(!defined('base_url')) define('base_url','http://localhost/omos/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_SERVER')) define('DB_SERVER',"vasimsqlserver99.mysql.database.azure.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"vasim@vasimsqlserver99");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"vA#sim123");
 if(!defined('DB_NAME')) define('DB_NAME',"omos_db");
 ?>
