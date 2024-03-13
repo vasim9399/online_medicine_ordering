abbc7fe3b78fd7bf5fef1433c935f9392b9ac976
vasim9399 <155420565+vasim9399@users.noreply.github.com>
2024-03-13 17:12:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,9 +22,9 @@
         include $page.'.php';
 
     }
-<?php require_once('inc/footer.php') ?>
-?>
 
+?>
+<?php require_once('inc/footer.php') ?>
   <div class="modal fade" id="uni_modal" role='dialog'>
     <div class="modal-dialog   rounded-0 modal-md modal-dialog-centered" role="document">
       <div class="modal-content  rounded-0">
